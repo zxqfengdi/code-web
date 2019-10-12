44f82de6bcb2bdb4d9037c1f2a4aab8d7abe7739
zxqfengdi <zxqfengdi@outlook.com>
2019-10-12 20:56:20 +0800

    add js exer code

---
+++ webAPI/common.js
@@ -0,0 +1,71 @@
+// 根据id获取元素
+
+function my$(id) {
+	return document.getElementById(id);
+}
+
+// 获取元素的文本内容
+
+function getInnerText(element) {
+	// 属性存在返回字符串'string'，不存在返回undefined
+	if (typeof element.innerText === 'string') {
+		return element.innerText;
+	} else {
+		return element.textContent;
+	}
+}
+
+// 获取元素的第一个子元素
+
+function getFirstElementChild(element) {
+	var node;
+	var nodes = element.childNodes;
+	var i = 0;
+	// 正序遍历
+	while (node = nodes[i++]) {
+		if (node.nodeType === 1) {
+			return node;
+		}
+	}
+	return null;
+}
+
+
+// 获取元素的最后一个子元素
+
+function getLastElementChild(element) {
+	var node;
+	var nodes = element.childNodes;
+	var i = nodes.length - 1;
+	// 倒序遍历
+	while (node = nodes[i--]) {
+		if (node.nodeType === 1) {
+			return node;
+		}
+	}
+	return null;
+}
+
+// 获取当前元素的前一个兄弟元素节点
+
+function getPreviousElementSibling(element) {
+	var el = element;
+	while (el = el.previousSibling) {
+		if (el.nodeType === 1) {
+			return el;
+		}
+	}
+	return null;
+}
+
+// 获取当前元素的后一个兄弟元素节点
+
+function getNextElementSibling(element) {
+	var el = element;
+	while (el = el.nextSibling) {
+		if (el.nodeType === 1) {
+			return el;
+		}
+	}
+	return null;
+}
