9e2592e09f939ec0b7bae487da3c789383718722
zxqfengdi <zxqfengdi@outlook.com>
2019-10-25 20:59:36 +0800

    add js exer code

--- project/04 贪吃蛇/js/food.js
+++ project/04 贪吃蛇/js/food.js
@@ -1,36 +1,61 @@
-// 食物对象构造函数
 
-function Food(options) {
-    options = options || {};  // 未传入options参数设置默认值为空对象
+// 使用匿名函数自调用创建局部作用域：避免命名冲突
+(function () {
+    // 食物对象构造函数
 
-    this.x = options.x || 0;
-    this.y = options.y || 0;
+    // 记录上次生成的食物
+    var elements = [];
 
-    this.width = options.width || 20;
-    this.height = options.height || 20;
+    function Food(options) {
+        options = options || {};  // 未传入options参数设置默认值为空对象
 
-    this.color = options.color || 'green';
-}
+        this.x = options.x || 0;
+        this.y = options.y || 0;
 
-// 食物渲染方法：食物对象显示到页面上(使用原型对象添加方法) map参数指向地图
-Food.prototype.render = function (map) {
+        this.width = options.width || 20;
+        this.height = options.height || 20;
 
-    // 随机设置位置
-    this.x = tools.getRandom(0, map.offsetWidth / this.width - 1) * this.width;
-    this.y = tools.getRandom(0, map.offsetHeight / this.height - 1) * this.height;
+        this.color = options.color || 'green';
+    }
 
-    // 创建食物对象添加到地图上
-    var div = document.createElement('div');
-    map.appendChild(div);
+    // 食物渲染方法：食物对象显示到页面上(使用原型对象添加方法) map参数指向地图
+    Food.prototype.render = function (map) {
+        // 删除之前的食物（蛇接触到食物时）
+        remove();
 
-    // 设置div 的样式
-    div.style.position = 'absolute';
-    div.style.left = this.x + 'px';
-    div.style.top = this.y + 'px';
-    div.style.width = this.width + 'px';
-    div.style.height = this.height + 'px';
-    div.style.backgroundColor = this.color;
-};
+        // 随机设置位置
+        this.x = tools.getRandom(0, map.offsetWidth / this.width - 1) * this.width;
+        this.y = tools.getRandom(0, map.offsetHeight / this.height - 1) * this.height;
+
+        // 创建食物对象添加到地图上
+        var div = document.createElement('div');
+        map.appendChild(div);
+
+        elements.push(div);
+
+        // 设置div 的样式
+        div.style.position = 'absolute';
+        div.style.left = this.x + 'px';
+        div.style.top = this.y + 'px';
+        div.style.width = this.width + 'px';
+        div.style.height = this.height + 'px';
+        div.style.backgroundColor = this.color;
+    };
+
+
+    // 删除食物方法
+    function remove() {
+        for (var i = elements.length - 1; i >= 0; i--) {
+            // 删除div
+            elements[i].parentNode.removeChild(elements[i]);
+            // 删除数组中的元素
+            elements.splice(i, 1);
+        }
+    }
+
+    window.Food = Food;
+})();
+
 
 // 测试
 var map = document.getElementById('map');
---
+++ project/04 贪吃蛇/js/snake.js
@@ -0,0 +1,46 @@
+(function () {
+    // 蛇对象构造函数
+    function Snake(options) {
+        options = options || {};
+
+        // 蛇节的大小
+        this.width = options.width || 20;
+        this.height = options.height || 20;
+
+        // 移动方向
+        this.direction = options.direction || 'right';
+
+        // 蛇的身体（初始三节，第一个为蛇头）
+        this.body = [
+            {x: 3, y: 2, color: 'red'},
+            {x: 2, y: 2, color: 'blue'},
+            {x: 1, y: 2, color: 'blue'}
+        ];
+    }
+
+    Snake.prototype.render = function (map) {
+        // 渲染每一个蛇节
+        for (var i = 0, len = this.body.length; i < len; i++) {
+            // 蛇节渲染
+            var object = this.body[i];
+
+            var div = document.createElement('div');
+            map.appendChild(div);
+
+            div.style.position = 'absolute';
+            div.style.width = this.width + 'px';
+            div.style.height = this.height + 'px';
+            div.style.left = object.x * this.width + 'px';
+            div.style.top = object.y * this.height + 'px';
+
+            div.style.backgroundColor = object.color;
+        }
+    }
+
+    window.Snake = Snake;
+})();
+
+// 蛇对象测试
+var snake = new Snake();
+var map = document.getElementById('map')
+snake.render(map);
